f260aa7f35b7cac16eef23762d1bdbf174c9d865
Matthew Welborn <mattgwelborn@gmail.com>
2019-10-09 10:26:30 -0400

    add checksum

---
+++ util/checksum.py
@@ -0,0 +1,8 @@
+import hashlib
+import sys
+
+b2b = hashlib.blake2b()
+with open(sys.argv[1], 'rb') as f:
+    for chunk in iter(lambda: f.read(8192), b""):
+        b2b.update(chunk)
+print(b2b.hexdigest())
