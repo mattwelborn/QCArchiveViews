6249a6a8b6619977b2dc154f1749aa17bea54de4
Matthew Welborn <mattgwelborn@gmail.com>
2019-10-10 20:10:12 -0400

    blob hash

--- util/checksum.py
+++ util/checksum.py
@@ -1,8 +1,9 @@
 import hashlib
+import json
 import sys
 
 b2b = hashlib.blake2b()
 with open(sys.argv[1], 'rb') as f:
     for chunk in iter(lambda: f.read(8192), b""):
         b2b.update(chunk)
-print(b2b.hexdigest())
+print(json.dumps({"blake2b": b2b.hexdigest()}))
